f400aba999831a16ae08eb210edbfce40c7f123f
Thomas Chartron <thomas.chartron@gmail.com>
2020-09-19 21:17:01 +0200

    :wrench: Add vue router import and config parameters

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Wape from './components/Wape.vue'
+
+Vue.use(VueRouter)
+
+Vue.config.silent = (process.env.NODE_ENV === 'production') ? true : false
+Vue.config.performance =  (process.env.NODE_ENV === 'production') ? false : true
+Vue.config.devtools =  (process.env.NODE_ENV === 'production') ? false : true
+Vue.config.productionTip =  (process.env.NODE_ENV === 'production') ? false : true
 
 export default {
     init(options) {
