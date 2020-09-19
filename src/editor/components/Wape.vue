cb8ff315b0ed82c1c491a47b900e9de9825a4f55
Thomas Chartron <thomas.chartron@gmail.com>
2020-09-19 11:12:00 +0200

    :sparkles: Vue main app component

---
+++ src/editor/components/Wape.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello vue'
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
