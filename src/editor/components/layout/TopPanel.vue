1e9a64ce79444236211ec1f0e5ea1e6300410a8c
Thomas Chartron <thomas.chartron@gmail.com>
2020-09-22 22:11:31 +0200

    :construction: TopPanel component

---
+++ src/editor/components/layout/TopPanel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="top-panel">
+    <h1>Top panel</h1>
+  </div>
+</template>
+
+<script>
+import TopPanel from 'Components/layout/TopPanel.vue'
+
+export default {
+    name: 'TopPanel',
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: blue;
+    }
+</style>
