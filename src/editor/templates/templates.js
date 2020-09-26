10872d9e71b4373889c07db3047e267e8041f1a8
Thomas Chartron <thomas.chartron@gmail.com>
2020-09-26 17:22:39 +0200

    :wrench: Droppable elements data sample from js file

---
+++ src/editor/templates/templates.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 1,
+        name: 'section',
+        title: 'Section',
+        icon: 'far fa-square',
+        content: `<div class="section"></div>`
+    },
+    {
+        id: 2,
+        name: 'two-columns',
+        title: 'Two Columns',
+        icon: 'fas fa-columns',
+        content: `<div class="two-columns">
+            <div class="column">
+            </div>
+            <div class="column">
+            </div>
+        </div>`
+    },
+    {
+        id: 3,
+        name: 'three-columns',
+        title: 'Three Columns',
+        icon: 'fas fa-border-all',
+        content: `<div class="three-columns">
+            <div class="column">
+            </div>
+            <div class="column">
+            </div>
+            <div class="column">
+            </div>
+        </div>`
+    },
+]
