65248ed59fafd7fc04533c1d9d2e9617b9b0e757
Thomas Chartron <thomas.chartron@gmail.com>
2020-10-04 20:30:14 +0200

    :wrench: Add iframecss var for HtmlWebpackPlugin in config/iframe.js

--- src/app/config/iframe.js
+++ src/app/config/iframe.js
@@ -1,5 +1,5 @@
 module.exports = {
     title: 'wape-content',
     bulma: 'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css',
-    css: 'src/app/styles/iframe.css'
+    iframecss: 'src/app/styles/iframe.css'
 }
