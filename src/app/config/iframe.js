ebd442956fa050df0f3546f9994618507e0374af
Thomas Chartron <thomas.chartron@gmail.com>
2020-10-05 22:22:52 +0200

    :arrow_up: Update minor bulma version into iframe

--- src/app/config/iframe.js
+++ src/app/config/iframe.js
@@ -1,5 +1,5 @@
 module.exports = {
     title: 'wape-content',
-    bulma: 'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css',
+    bulma: 'https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css',
     iframecss: 'src/app/styles/iframe.css'
 }
