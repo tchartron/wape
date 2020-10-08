32905b381a1335321ddbef5e556493d5e1521bb6
Thomas Chartron <thomas.chartron@gmail.com>
2020-10-08 23:26:20 +0200

    :construction: Building elements js file

--- src/editor/elements/elements.js
+++ src/editor/elements/elements.js
@@ -3,16 +3,39 @@ export default [
         title: 'typography',
         elements: [
             {
-                id: 1,
+                id: 5,
                 title: 'Heading',
                 icon: 'fas fa-heading',
-                content: `<h1 class="title is-1">Title 1</h1>`
+                droppable: false,
+                content: `<h1 class="">Title H1</h1>`
             },
             {
-                id: 2,
-                title: 'Subtitle',
-                icon: 'fas fa-heading',
-                content: `<h2 class="subtitle is-2">Subtitle 2</h2>`
+                id: 6,
+                title: 'Paragraph',
+                icon: 'fas fa-align-left',
+                droppable: false,
+                content: `<p class="">Paragraph</p>`
+            },
+            {
+                id: 7,
+                title: 'Text Block',
+                icon: 'fas fa-align-justify',
+                droppable: false,
+                content: `<div class="">This is some text you should replace</div>`
+            },
+            {
+                id: 8,
+                title: 'Text Link',
+                icon: 'fas fa-link',
+                droppable: false,
+                content: `<a href="#" class="">This is a link</a>`
+            },
+            {
+                id: 9,
+                title: 'Block quote',
+                icon: 'fas fa-quote-right',
+                droppable: false,
+                content: `<blockquote class="">This is a link</blockquote>`
             }
         ],
     },
@@ -20,16 +43,18 @@ export default [
         title: 'media',
         elements: [
             {
-                id: 1,
-                title: 'Heading',
-                icon: 'fas fa-heading',
-                content: `<h1 class="title is-1">Title 1</h1>`
+                id: 10,
+                title: 'Image',
+                icon: 'far fa-image',
+                droppable: false,
+                content: `<img src="" class="title is-1" />`
             },
             {
-                id: 2,
-                title: 'Subtitle',
-                icon: 'fas fa-heading',
-                content: `<h2 class="subtitle is-2">Subtitle 2</h2>`
+                id: 11,
+                title: 'Video',
+                icon: 'fas fa-video',
+                droppable: false,
+                content: `<video src=""></video>`
             }
         ],
     },
@@ -37,16 +62,18 @@ export default [
         title: 'social',
         elements: [
             {
-                id: 1,
-                title: 'Heading',
-                icon: 'fas fa-heading',
-                content: `<h1 class="title is-1">Title 1</h1>`
+                id: 12,
+                title: 'Facebook',
+                icon: 'fab fa-facebook-f',
+                droppable: false,
+                content: `<a href="#" class="">This is a link</a>`
             },
             {
-                id: 2,
-                title: 'Subtitle',
-                icon: 'fas fa-heading',
-                content: `<h2 class="subtitle is-2">Subtitle 2</h2>`
+                id: 13,
+                title: 'Twitter',
+                icon: 'fab fa-twitter',
+                droppable: false,
+                content: `<a href="#" class="">This is a link</a>`
             }
         ],
     }
