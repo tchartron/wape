f6a7686914a3e7988fd41cfea706eaa621860d38
Thomas Chartron <thomas.chartron@gmail.com>
2020-10-08 22:05:01 +0200

    :wrench: Add basic droppable containers

--- src/editor/elements/layouts.js
+++ src/editor/elements/layouts.js
@@ -4,39 +4,27 @@ export default [
         title: 'Section',
         icon: 'far fa-square',
         droppable: true,
-        content: `<section class="section droppable"></section>`
+        content: `<div class="container droppable"></div>`
     },
     {
         id: 2,
         title: 'Container',
         icon: 'far fa-square',
         droppable: true,
-        content: `<div class="container droppable"></div>`
+        content: `<div class="container px-10 droppable"></div>`
     },
     {
         id: 3,
         title: 'Columns',
         icon: 'fas fa-columns',
         droppable: true,
-        content: `<div class="columns">
-            <div class="column">
-            </div>
-            <div class="column">
-            </div>
-        </div>`
+        content: `<div class="grid grid-cols-2 gap-2 droppable"><div></div><div></div></div>`
     },
     {
         id: 4,
         title: 'Grid',
         icon: 'fas fa-border-all',
         droppable: true,
-        content: `<div class="three-columns">
-            <div class="column">
-            </div>
-            <div class="column">
-            </div>
-            <div class="column">
-            </div>
-        </div>`
+        content: `<div class="grid grid-cols-2 gap-2 droppable"><div></div><div></div><div></div><div></div></div>`
     },
 ]
