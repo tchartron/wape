bc52ed5a48ff0e48c9d5a0e1d70305b9bc377ce2
Thomas Chartron <thomas.chartron@gmail.com>
2020-10-08 20:14:14 +0200

    :art: Element js class

---
+++ src/editor/Element.js
@@ -0,0 +1,40 @@
+export default class Element {
+    constructor(id, title, icon, droppable, content) {
+        this._id = id //int unique
+        this._title = title //title shown in leftmenu
+        this._icon = icon //icon shown in leftmenu
+        this._droppable = droppable //bool droppable or not (containers are droppable, elements are not)
+        this._content = content //string html content
+    }
+    get id() {
+        return this._id
+    }
+    get title() {
+        return this._title
+    }
+    get icon() {
+        return this._icon
+    }
+    get droppable() {
+        return this._droppable
+    }
+    get content() {
+        return this._content
+    }
+
+    set id(id) {
+        this._id = id
+    }
+    set title(title) {
+        this._title = title
+    }
+    set icon(icon) {
+        this._icon = icon
+    }
+    set droppable(droppable) {
+        this._droppable = droppable
+    }
+    set content(content) {
+        this._content = content
+    }
+}
