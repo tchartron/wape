12967ec28503f2ee89cced88eebf6419d6b9f59d
Thomas Chartron <thomas.chartron@gmail.com>
2021-02-23 21:37:43 +0100

    :art: New editor config file

---
+++ src/editor/config/editor.js
@@ -0,0 +1,13 @@
+const medias = {
+    desktop: {
+        width: 'calc(100vw - 500px)'
+    },
+    tablet: {
+        width: '768px'
+    },
+    mobile: {
+        width: '375px'
+    },
+}
+
+export { medias }
