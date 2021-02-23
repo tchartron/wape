7e0afac50dcdac44f70cb6a68cb57eb457909ddf
Thomas Chartron <thomas.chartron@gmail.com>
2021-02-23 21:37:23 +0100

    :art: Import utilities from the newly created files into Dragger.js class

--- src/editor/Dragger/Dragger.js
+++ src/editor/Dragger/Dragger.js
@@ -1,6 +1,7 @@
 // import mitt from 'mitt'
 // import Event from './Event'
 // export const emitter = mitt()
+import { countRows, countCols } from 'Editor/utilities/grid'
 
 export class Dragger {
     constructor(draggableSelector, options = {}) {
@@ -324,37 +325,4 @@ export class Dragger {
     elementExistsInContainer(container, element) {
         return container.contains(element)
     }
-}
-
-export function countRows(element) {
-    let classList = element.className.split(' ')
-    let rowsAmount = 0
-    if(classList.includes('grid')) {
-        let rowsClass = classList.find((elem) => {
-            return (elem.match(/grid-rows-([0-9])/) !== null)
-        })
-        if(typeof rowsClass !== 'undefined') {
-            let matchRows = rowsClass.match(/grid-rows-([0-9])/)
-            if(matchRows !== null) {
-                rowsAmount = matchRows[1]
-            }
-        }
-    }
-    return parseInt(rowsAmount)
-}
-export function countCols(element) {
-    let classList = element.className.split(' ')
-    let colsAmount = 0
-    if(classList.includes('grid')) {
-        let colsClass = classList.find((elem) => {
-            return (elem.match(/grid-cols-([0-9])/) !== null)
-        })
-        if(typeof colsClass !== 'undefined') {
-            let matchCols = colsClass.match(/grid-cols-([0-9])/)
-            if(matchCols !== null) {
-                colsAmount = matchCols[1]
-            }
-        }
-    }
-    return parseInt(colsAmount)
 }
