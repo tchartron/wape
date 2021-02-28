65a0d0d4895bcdf18275d63ff01cf00fff8309e6
Thomas Chartron <thomas.chartron@gmail.com>
2021-02-28 10:14:44 +0100

    :construction: Container type in Container class

--- src/editor/Container.js
+++ src/editor/Container.js
@@ -3,6 +3,7 @@ import { countRows, countCols } from 'Editor/utilities/grid'
 export default class Container {
     constructor(element) {
         this.element = element // html element
+        this.type = this.containerType(element)
         this.rows = countRows(element)
         this.cols = countCols(element)
     }
@@ -13,7 +14,9 @@ export default class Container {
     // set element(element) {
     //     this.element = element
     // }
-
+    containerType(element) { //Determine wether it's a grid or a simple container to allow responsive options display
+        return (element.matches('.grid')) ? 'grid' : 'container'
+    }
     addClass(cssClass) {
         this.element.classList.add(cssClass)
     }
