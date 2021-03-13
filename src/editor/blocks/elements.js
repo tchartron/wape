b2d3eda82b8ba8614fdf3a439fd519a9df184a88
Thomas Chartron <thomas.chartron@gmail.com>
2021-03-13 22:48:52 +0100

    :wrench: Add type element to elements.js to reflect layouts.js objects structure

--- src/editor/blocks/elements.js
+++ src/editor/blocks/elements.js
@@ -6,30 +6,35 @@ export default [
                 id: 5,
                 title: 'Heading',
                 icon: 'fas fa-heading',
+                type: 'element',
                 content: `<h1 class="">Title H1</h1>`
             },
             {
                 id: 6,
                 title: 'Paragraph',
                 icon: 'fas fa-align-left',
+                type: 'element',
                 content: `<p class="">Paragraph</p>`
             },
             {
                 id: 7,
                 title: 'Text Block',
                 icon: 'fas fa-align-justify',
+                type: 'element',
                 content: `<div class="">This is some text you should replace</div>`
             },
             {
                 id: 8,
                 title: 'Text Link',
                 icon: 'fas fa-link',
+                type: 'element',
                 content: `<a href="#" class="">This is a link</a>`
             },
             {
                 id: 9,
                 title: 'Block quote',
                 icon: 'fas fa-quote-right',
+                type: 'element',
                 content: `<blockquote class="">This is a link</blockquote>`
             }
         ],
@@ -41,12 +46,14 @@ export default [
                 id: 10,
                 title: 'Image',
                 icon: 'far fa-image',
+                type: 'element',
                 content: `<img src="" class="title is-1" />`
             },
             {
                 id: 11,
                 title: 'Video',
                 icon: 'fas fa-video',
+                type: 'element',
                 content: `<video src=""></video>`
             }
         ],
@@ -58,12 +65,14 @@ export default [
                 id: 12,
                 title: 'Facebook',
                 icon: 'fab fa-facebook-f',
+                type: 'element',
                 content: `<a href="#" class="">Facebook</a>`
             },
             {
                 id: 13,
                 title: 'Twitter',
                 icon: 'fab fa-twitter',
+                type: 'element',
                 content: `<a href="#" class="">Twitter</a>`
             }
         ],
